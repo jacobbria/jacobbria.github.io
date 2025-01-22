cfd06a489fb1165b14ceab3db4657671cc100a7e
jacobbria <jacob.bria@sinclair.edu>
2025-01-21 21:07:00 -0500

    Fixed layout so header background unaffected by the display:none on div.

--- javascript/blog.js
+++ javascript/blog.js
@@ -14,50 +14,45 @@ const client = contentful.createClient({ // <-- instantiates contentful object v
   });
 
   // Fetch entries "Blog Post"
-  client.getEntries({ content_type: 'resumeBlogPost' }) // <-- retrieves JSON object from API
-    .then(response => {
-      const articles = response.items;
-      const cardsContainer = document.getElementById('cards-container');
-  
-      articles.forEach(article => {
-        const { title, datePosted, blogContent } = article.fields;
-        
-        // parse/format blog content with Marked Library
-        const formattedContent = marked.parse(blogContent); 
-        
-        // Create  card
-        const card = document.createElement('div');
-        card.className = 'card';
-        card.innerHTML = `
-           <div class="title_container">
-              <div class="title_section">
-                <h3 class="title_blog">${title}</h3>
-              </div>
-              <div class="icon_container">
-                <span class="toggle_icon"><i class="fas fa-chevron-down"></i></span>
-              </div>
-          </div>
-          <p><strong>Date Posted:</strong> ${new Date(datePosted).toDateString()}</p>
-          <div class="content hidden" id="content"> <p>${formattedContent}</p> </div>
-        `;
-  
-        // Append card to blog page
-        cardsContainer.appendChild(card);
+  client.getEntries({ content_type: 'resumeBlogPost' })
+  .then(response => {
+    const articles = response.items;
+    const cardsContainer = document.getElementById('cards-container');
 
-        
-      // Control Expand Icon and content when clicked
-      const toggleIcon = document.querySelector('.toggle_icon');
-      const contentDiv = document.querySelector('.content');
+    articles.forEach(article => {
+      const { title, datePosted, blogContent } = article.fields;
+      const formattedContent = marked.parse(blogContent);
+
+      // Create card element
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+        <div class="title_container">
+          <div class="title_section">
+            <h3 class="title_blog">${title}</h3>
+          </div>
+          <div class="icon_container">
+            <span class="toggle_icon"><i class="fas fa-chevron-down"></i></span>
+          </div>
+        </div>
+        <p><strong>Date Posted:</strong> ${new Date(datePosted).toDateString()}</p>
+        <div class="content hidden" id="content" >
+          <p>${formattedContent}</p>
+        </div>
+      `;
+
+      // Append card to the container
+      cardsContainer.appendChild(card);
+
+      // Set up event listener for the toggle icon
+      const toggleIcon = card.querySelector('.toggle_icon');
+      const contentDiv = card.querySelector('.hidden');
 
       toggleIcon.addEventListener('click', () => {
-        console.log("Rotate clicked!");
-        toggleIcon.classList.toggle('rotated');  // Toggle the 'rotated' class on click
-        contentDiv.classList.toggle('hidden');
+        toggleIcon.classList.toggle('rotated');
+        contentDiv.classList.toggle('hidden'); /* <-- this affects the hidden class to help prevent 2x clicks */
+        
       });
-      
     });
-    })
-    .catch(error => console.error('Error fetching Contentful data:', error));
-
-      
-  
+  })
+  .catch(error => console.error('Error fetching Contentful data:', error));
